ac914b643074d3bfb6b708b6af1c0e303e4a92d7
rasmusfabius <rasmusfabius@gmail.com>
2019-11-23 19:49:58 +0100

    Project finish

---
+++ pokemon/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Pokegame from './Pokegame';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Pokegame />
+    </div>
+  );
+}
+
+export default App;
